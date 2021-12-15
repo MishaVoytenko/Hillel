0bca837cfb7407da35abfe242b7eb54b4acaa6ee
BuildTools <unconfigured@null.spigotmc.org>
2021-12-15 23:53:10 +0200

    corrected

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-var name = prompt("привет, как тебя зовут?");
-if (name) {
-  alert("Hello, " + name + " How are you?");
-}
-name = prompt("привет, как тебя зовут?");
+const name = prompt("привет, как тебя зовут?");
+alert("Hello, " + name + " How are you?");
